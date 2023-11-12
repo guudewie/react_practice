b716433beb4ad5f83393213d0a463786781b946a
guudewie <philippthiessen12@gmail.com>
2023-11-12 13:34:24 +0100

    write conditional rendering logic

---
+++ src/List.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+function List(props) {
+  if (!props.animals) {
+    return <div>Loading...</div>;
+  }
+
+  if (props.animals.length === 0) {
+    return <div>There are no animals in the list!</div>;
+  }
+
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return <li key={animal}>{animal}</li>;
+      })}
+    </ul>
+  );
+}
+
+export default List;
